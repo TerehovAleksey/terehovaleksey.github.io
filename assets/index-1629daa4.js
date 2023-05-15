979772d1d521f61e45419764b58557e2226637e6
TerehovAleksey <375295565021@yandex.ru>
2023-05-15 05:51:07 +0000

    Deploying to main from @ TerehovAleksey/my-cv-react@0a87d285af8a7d3ed2605da38486e0e6be722b98 🚀

---
+++ assets/index-1629daa4.js
@@ -0,0 +1,1 @@
+import{j as a,L as s}from"./index-e59b873f.js";import{u as o,b as i,a as g}from"./index-0c6debb8.js";const c=()=>{const{cv:e,img:t,language:r}=o();return a("div",{style:{height:"calc(100vh - 20px)"},children:e&&a(i,{width:"100%",height:"100%",showToolbar:!0,children:a(g,{cv:e,img:t,lang:s[r]})})})};export{c as default};
